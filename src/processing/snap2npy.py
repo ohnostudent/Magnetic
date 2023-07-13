17c6c78cf94e80d74376de3dd7a9b2890b121e8d
kuni <navy.d7ed7ish@gmail.com>
2023-07-13 21:59:58 +0900

    update too

--- src/processing/snap2npy.py
+++ src/processing/snap2npy.py
@@ -12,62 +12,90 @@ def snap2npy(sp: SnapData, path: str, dataset: int):
     # print(data.shape)
 
     _snap_all(path, dataset, data)
-    _snap_half(path, dataset, data)
+    _snap_half_left(path, dataset, data)
+    _snap_half_right(path, dataset, data)
 
 
-def _snap_half(path, dataset, data):
-    out_path_half = path.replace(f"snap{dataset}", f"half/snap{dataset}")
+def _snap_half_left(path, dataset, data):
+    # 保存先のpath の作成
+    out_path_half = path.replace(f"snap{dataset}", f"half_left/snap{dataset}")
     if not os.path.exists(os.path.dirname(out_path_half)):
         os.makedirs(os.path.dirname(out_path_half))
 
-    half = data[256: 1025-256, :]
+    # 縦625 * 横256 に切り取る
+    half = data[200: 1025-200, :257]
+
+    # numpy 配列として保存
+    np.save(out_path_half, half)
+
+
+def _snap_half_right(path, dataset, data):
+    # 保存先のpath の作成
+    out_path_half = path.replace(f"snap{dataset}", f"half_right/snap{dataset}")
+    if not os.path.exists(os.path.dirname(out_path_half)):
+        os.makedirs(os.path.dirname(out_path_half))
+
+    # 縦625 * 横256 に切り取る
+    half = data[200: 1025-200, 257:]
+    # numpy 配列として保存
     np.save(out_path_half, half)
 
 
 def _snap_all(path, dataset, data):
+    # 保存先のpath の作成
     out_path_all = path.replace(f"snap{dataset}", f"all/snap{dataset}")
     if not os.path.exists(os.path.dirname(out_path_all)):
         os.makedirs(os.path.dirname(out_path_all))
 
+    # numpy 配列として保存
     np.save(out_path_all, data)
 
 
-def doSnap2npy():
+def doSnap2npy(dataset):
     from glob import glob
     from params import SNAP_PATH, datasets, variable_parameters
     from SetLogger import logger_conf
-
-
-    sp = SnapData()
     logger = logger_conf()
-    logger.debug("START", extra={"addinfo": f"処理開始"})
+    logger.debug("START", extra={"addinfo": f"処理開始\n"})
+
+    if dataset not in datasets:
+        logger.debug("ERROR", extra={"addinfo": f"cannot use dataset{dataset}\n"})
+        return
 
     try:
-        for dataset in datasets:
-            logger.debug("START", extra={"addinfo": f"Snap{dataset} 開始"})
+        sp = SnapData()
+        logger.debug("START", extra={"addinfo": f"Snap{dataset} 開始\n"})
 
-            for param in variable_parameters:
-                logger.debug("START", extra={"addinfo": f"{param} 開始"})
+        for param in variable_parameters:
+            logger.debug("START", extra={"addinfo": f"{param} 開始"})
 
-                for path in glob(SNAP_PATH + f"/snap{dataset}/{param}/*/*"):
-                    # print(path)
-                    snap2npy(sp, path, dataset)
+            for path in glob(SNAP_PATH + f"/snap{dataset}/{param}/*/*"):
+                # print(path)
+                snap2npy(sp, path, dataset)
 
-                logger.debug("END", extra={"addinfo": f"{param} 終了"})
-            logger.debug("END", extra={"addinfo": f"Snap{dataset} 終了"})
+            logger.debug("END", extra={"addinfo": f"{param} 終了\n"})
+        logger.debug("END", extra={"addinfo": f"Snap{dataset} 終了\n"})
     
     except KeyboardInterrupt:
-        logger.debug("END", extra={"addinfo": f"{param} 中断"})
+        logger.debug("END", extra={"addinfo": f"{param} 中断\n"})
         logger.debug("END", extra={"addinfo": f"Snap{dataset} 中断\n"})
     
     except Exception as e:
         logger.debug(str(e), extra={"addinfo": "\n"})
-        logger.debug("END", extra={"addinfo": f"{param} 中断"})
+        logger.debug("END", extra={"addinfo": f"{param} 中断\n"})
         logger.debug("END", extra={"addinfo": f"Snap{dataset} 中断\n"})
     
     finally:
         logger.debug("END", extra={"addinfo": f"処理終了"})
 
+def main():
+    from concurrent.futures import ThreadPoolExecutor
+
+    with ThreadPoolExecutor(max_workers=6) as exec:
+        exec.submit(doSnap2npy, 77)
+        exec.submit(doSnap2npy, 497)
+        exec.submit(doSnap2npy, 4949)
+
 
 if __name__ == "__main__":
-    doSnap2npy()
+    main()
