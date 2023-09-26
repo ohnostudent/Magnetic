6d2b7b34ceb2584bf438e65a312d85eac50df645
kuni <kuni.saysis.009@gmail.com>
2023-09-26 17:17:32 +0900

    Update .bat for making base folders

--- cmd/BasePath.bat
+++ cmd/BasePath.bat
@@ -1,4 +1,28 @@
-mkdir imgout
-mkdir logs
-mkdir snaps
-mkdir ML
+setlocal ENABLEDELAYEDEXPANSION
+@echo off
+
+mkdir data images logs snaps
+mkdir ML\data ML\models ML\result
+mkdir ML\data\LIC_labels ML\data\snpa_files
+mkdir ML\models\npz ML\models\model
+
+cd snaps
+
+set dataset=77 497 4949
+for %%d in (%dataset%) do (
+	for /l %%n in (0,1,14) do (
+        set num=0%%n
+        set num=!num:~-2,2!
+
+        mkdir snap%%d\density\!num!
+        mkdir snap%%d\pressure\!num!
+        mkdir snap%%d\velocityx\!num!
+        mkdir snap%%d\velocityy\!num!
+        mkdir snap%%d\velocityz\!num!
+        mkdir snap%%d\magfieldx\!num!
+        mkdir snap%%d\magfieldy\!num!
+        mkdir snap%%d\magfieldz\!num!
+        mkdir snap%%d\enstrophy\!num!
+	)
+)
+endlocal
--- cmd/createVenv.bat
+++ cmd/createVenv.bat
@@ -1,1 +1,3 @@
 python -m venv .venv
+.venv\Scripts\activate
+pip install -r requirements.txt
